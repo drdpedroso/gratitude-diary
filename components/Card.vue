784b6e5a8ce6daa1d85a5c507f8b21aea3418526
Eduardo <drdpedroso@gmail.com>
2019-02-26 08:43:33 +0100

    Add list component and card component

---
+++ components/Card.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="card">
+    <slot />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Card'
+}
+</script>
+
+<style scoped>
+.card {
+  text-align: center;
+  margin-bottom: 30px;
+}
+</style>
---
+++ components/List.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="list-wrapper">
+    <card v-for="item in items" :key="item.id">
+      <div v-if="!item.done" class="control">
+        <textarea v-model="item.text" class="textarea has-fixed-size" placeholder="Fixed size textarea" />
+      </div>
+      <div v-else>
+        <span class="done-item">{{ item.text }}</span>
+      </div>
+    </card>
+  </div>
+</template>
+
+<script>
+import card from './Card'
+
+export default {
+  name: 'List',
+  components: {
+    card
+  },
+  data() {
+    return {
+      items: [
+        { id: 1, text: 'Something grateful and stuff', done: true },
+        { id: 2, text: 'Something grateful and another stuff', done: false },
+        { id: 3, text: 'Something another stuff', done: false }
+      ]
+    }
+  }
+}
+</script>
+
+<style scoped>
+.list-wrapper {
+  display: flex;
+  text-space: 10px;
+  flex-direction: column;
+}
+
+.list-wrapper > div {
+  padding: 40px;
+}
+
+.done-item {
+  font-size: 20px;
+}
+</style>
